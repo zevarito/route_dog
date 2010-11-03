ed5f332b43ca902f7825bbfb0d82af99d3ab7a93
Alvaro Gil <zevarito@gmail.com>
2010-11-03 11:43:27 -0200

    load middlewares through middlewares

--- lib/route_dog/railtie.rb
+++ lib/route_dog/railtie.rb
@@ -3,5 +3,14 @@ module RouteDog
     rake_tasks do
       load "tasks/tasks.rake"
     end
+
+    initializer "route_dog.configure_rails_initialization" do |app|
+      setup_middlewares(app)
+    end
+
+    def setup_middlewares(app)
+      app.config.middleware.use RouteDog::Middleware::Watcher  if Rails.env.test?
+      app.config.middleware.use RouteDog::Middleware::Notifier if Rails.env.development? || Rails.env.test?
+    end
   end
 end
--- test/mock_app/config/initializers/route_dog.rb
+++
@@ -1,6 +0,0 @@
-# Insert this middleware to know from tests what routes aren't tested
-Rails.application.config.middleware.use RouteDog::Middleware::Watcher  if Rails.env.test?
-
-# Insert this middleware in development to append html to let the user know if the route is not tested
-# Also we add this middleware to test env so that way we can test the whole thing :D
-Rails.application.config.middleware.use RouteDog::Middleware::Notifier if Rails.env.test? || Rails.env.development?
