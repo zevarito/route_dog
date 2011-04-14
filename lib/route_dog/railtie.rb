c7b59556a7af475695c0f92cc19a51579514f5fd
Alvaro Gil <zevarito@gmail.com>
2011-04-14 18:49:35 -0300

    refactor a little bit how the middlewares are loaded

--- lib/route_dog/railtie.rb
+++ lib/route_dog/railtie.rb
@@ -1,27 +1,30 @@
 module RouteDog
   class Railtie < Rails::Railtie
-    attr_reader :route_dog_config
 
     rake_tasks do
       load "tasks/tasks.rake"
     end
 
     initializer "route_dog.configure_rails_initialization" do |app|
-      load_route_dog_configuration
       setup_middlewares(app)
     end
 
   private
 
     def setup_middlewares(app)
-      app.config.middleware.use RouteDog::Middleware::Watcher  if route_dog_config.has_key?("watcher") && route_dog_config["watcher"]["env"].include?(Rails.env)
-      app.config.middleware.use RouteDog::Middleware::Notifier if route_dog_config.has_key?("notifier") && route_dog_config["notifier"]["env"].include?(Rails.env)
+      if route_dog_configuration.fetch("watcher", {}).fetch("env", []).include?(Rails.env)
+        app.config.middleware.use RouteDog::Middleware::Watcher
+      end
+
+      if route_dog_configuration.fetch("notifier", {}).fetch("env", []).include?(Rails.env)
+        app.config.middleware.use RouteDog::Middleware::Notifier
+      end
     end
 
-    def load_route_dog_configuration
-      @route_dog_config ||= YAML.load_file(File.join(Rails.root, 'config', 'route_dog.yml'))
+    def route_dog_configuration
+      YAML.load_file(File.join(Rails.root, 'config', 'middlewares_route_dog.yml'))
     rescue Errno::ENOENT
-      @route_dog_config = {"watcher" => {"env" => ["test"]}, "notifier" => {"env" => ["development"]}}
+      {"watcher" => {"env" => ["test"]}, "notifier" => {"env" => ["development"]}}
     end
   end
 end
