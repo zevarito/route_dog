f80b865b93b2d857b8c5cee7270447b0c7a21c5c
Alvaro Gil <zevarito@gmail.com>
2011-04-14 19:08:38 -0300

    refactor notifier middleware

--- lib/route_dog/middleware/notifier.rb
+++ lib/route_dog/middleware/notifier.rb
@@ -19,21 +19,16 @@ module RouteDog
 
       def append_warning
         @response.each do |part|
-          part.gsub!("<body>", "<body>#{warning_html}")
+          part.gsub!("<body>", "<body>#{warning_template}")
         end
       end
 
       def no_test_message
-        "The Route #{request_method.to_s.upcase} #{identify_controller.to_s}##{identify_action.to_s} -- Has Not Integrational Tests!"
+        "The Route #{request_method.to_s.upcase} #{identify_controller.to_s}##{identify_action.to_s} -- Has Not Integration Tests!"
       end
 
-      def warning_html
-        <<-EOT
-          <div style="display: block; height:70px;"></div>
-          <div id="route_dog_warning" style="display: block; width: 100%; height: 70px; text-align: center; margin:0; position:absolute; top:0; background: red; font-size: 18px; font-weight: bold">
-            <h1 style="color: #fff; font-size: 20px; margin: 20px 0 35px">#{no_test_message}</h1>
-          </div>
-        EOT
+      def warning_template
+        ERB.new(File.open(File.join(File.dirname(__FILE__), "..", "templates", "warning.html.erb")).read).result(binding)
       end
     end
   end
