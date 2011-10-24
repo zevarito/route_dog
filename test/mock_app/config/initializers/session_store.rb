a2b4b37a16df2e1a605a401636aaf8022436c0fd
Alvaro Gil <zevarito@gmail.com>
2011-10-24 11:45:04 -0200

    upgrade rails mock app to 3.1

--- test/mock_app/config/initializers/session_store.rb
+++ test/mock_app/config/initializers/session_store.rb
@@ -4,5 +4,5 @@ MockApp::Application.config.session_store :cookie_store, :key => '_mock_app_sess
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # MockApp::Application.config.session_store :active_record_store
